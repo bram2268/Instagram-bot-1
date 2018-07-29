83e81ff35f221a084e735cd1c8e865164f0286a3
bram2268 <bram2268@gmail.com>
2018-07-29 13:49:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from src.follow_protocol import follow_protocol
 from src.unfollow_protocol import unfollow_protocol
 
 bot = InstaBot(
-    login="instantlfollows",
-    password="Qaz12wsx",
+    login="user",
+    password="pass",
     like_per_day=700,
     comments_per_day=0,
     tag_list=['follow', 'followme', 'followback', 'followforfollow'],
